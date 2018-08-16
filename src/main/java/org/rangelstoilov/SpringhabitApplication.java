c6ae231daa31eed24bc437c3f4e53c96d910ef49
rangel <RangelStoilov@students.softuni.bg>
2018-08-16 10:08:53 +0300

    Initial commit

---
+++ src/main/java/org/rangelstoilov/SpringhabitApplication.java
@@ -0,0 +1,12 @@
+package org.rangelstoilov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhabitApplication.class, args);
+	}
+}
